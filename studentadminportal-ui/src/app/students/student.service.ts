adc677ddc904f1b18337bb0144cadd5a39ecbbee
vishalkr <itsvishalkr@gmail.com>
2022-02-12 21:59:56 +0530

    Final commit

--- studentadminportal-ui/src/app/students/student.service.ts
+++ studentadminportal-ui/src/app/students/student.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { env } from 'process';
+
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { AddStudentRequest } from '../models/api-models/add-student-request.model';
